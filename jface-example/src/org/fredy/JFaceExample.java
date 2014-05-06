b0f65f8c9b922c7f54291bca4829684aff68ecaf
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-05-06 16:20:00 -0500

    Add dialog examples

--- jface-example/src/org/fredy/JFaceExample.java
+++ jface-example/src/org/fredy/JFaceExample.java
@@ -21,12 +21,24 @@ import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.ColorDialog;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.FontDialog;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
@@ -38,6 +50,7 @@ public class JFaceExample {
         new TreeViewerExample();
         new TextViewerExample();
         new ComboViewerExample();
+        new DialogsExample();
     }
 
     private static class ListViewExample {
@@ -322,4 +335,94 @@ public class JFaceExample {
             display.dispose();
         }
     }
+    
+    private static class DialogsExample {
+        public DialogsExample() {
+            Display display = new Display();
+            Shell shell = new Shell(display);
+            shell.setText("Dialogs Example");
+            
+            GridLayout layout = new GridLayout(1, false);
+            Composite composite = new MyComposite(shell, SWT.BORDER);
+            GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
+            composite.setLayoutData(gridData);
+            shell.setLayout(layout);
+            
+            shell.pack();
+            shell.open();
+            while (!shell.isDisposed()) {
+                if (!display.readAndDispatch())
+                    display.sleep();
+            }
+            display.dispose();
+        }
+        
+        private static class MyComposite extends Composite {
+            public MyComposite(final Composite parent, int style) {
+                super(parent, style);
+                setLayout(new GridLayout(1, false));
+
+                Button button = new Button(this, SWT.PUSH);
+                button.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+                button.setText("Press me");
+                button.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        Shell shell = parent.getShell();
+                        openDialogs(shell);
+                    }
+                });
+            }
+
+            private void openDialogs(Shell shell) {
+                // File standard dialog
+                FileDialog fileDialog = new FileDialog(shell);
+                // Set the text
+                fileDialog.setText("Select File");
+                // Set filter on .txt files
+                fileDialog.setFilterExtensions(new String[] { "*.txt" });
+                // Put in a readable name for the filter
+                fileDialog.setFilterNames(new String[] { "Textfiles(*.txt)" });
+                // Open Dialog and save result of selection
+                String selected = fileDialog.open();
+                System.out.println(selected);
+
+                // Directly standard selection
+                DirectoryDialog dirDialog = new DirectoryDialog(shell);
+                dirDialog.setText("Select your home directory");
+                String selectedDir = dirDialog.open();
+                System.out.println(selectedDir);
+
+                // Select Font
+                FontDialog fontDialog = new FontDialog(shell);
+                fontDialog.setText("Select your favorite font");
+                FontData selectedFont = fontDialog.open();
+                System.out.println(selectedFont);
+
+                // Select Color
+                ColorDialog colorDialog = new ColorDialog(shell);
+                colorDialog.setText("Select your favorite color");
+                RGB selectedColor = colorDialog.open();
+                System.out.println(selectedColor);
+
+                // Message
+                MessageBox messageDialog = new MessageBox(shell, SWT.ERROR);
+                messageDialog.setText("Evil Error has happend");
+                messageDialog.setMessage("This is fatal!!!");
+                int returnCode = messageDialog.open();
+                System.out.println(returnCode);
+
+                // Message with ok and cancel button and info icon
+                messageDialog = new MessageBox(shell, 
+                    SWT.ICON_QUESTION | 
+                    SWT.OK
+                    | SWT.CANCEL);
+                messageDialog.setText("My info");
+                messageDialog.setMessage("Do you really want to do this.");
+                returnCode = messageDialog.open();
+                System.out.println(returnCode);
+            }
+        } 
+    }
+    
 }
