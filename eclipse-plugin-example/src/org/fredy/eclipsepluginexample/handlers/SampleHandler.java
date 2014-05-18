b239f2162bae71827459465c592082060562e02a
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-05-17 20:14:45 -0700

    Replace Action with Handler

---
+++ eclipse-plugin-example/src/org/fredy/eclipsepluginexample/handlers/SampleHandler.java
@@ -0,0 +1,20 @@
+package org.fredy.eclipsepluginexample.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class SampleHandler extends AbstractHandler {
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+        MessageDialog.openInformation(
+            window.getShell(),
+            "Eclipse-plugin-example",
+            "Hello, Eclipse world");
+        return null;
+    }
+}
